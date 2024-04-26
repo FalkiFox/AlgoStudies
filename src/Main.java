b0affd9eaaee1e9a9dff88614e79f7a188291c23
FalkiFox <juwol1423@gmail.com>
2024-04-26 22:26:07 +0200

    Code cleanup

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ package src;
 public class Main {
     public static void main(String[] args) {
         SortAnalyzer analysis = new SortAnalyzer(true);
-
-        System.out.println("Worst Case");
         analysis.insertionSort_setWorstCase(5);
         System.out.println(analysis.insertionSort_MoveApproach());
     }
--- src/SortAnalyzer.java
+++ src/SortAnalyzer.java
@@ -3,7 +3,7 @@ package src;
 import java.util.ArrayList;
 
 public class SortAnalyzer {
-    private ArrayList<Integer> unsortedArray = new ArrayList<>();
+    private ArrayList<Integer> array = new ArrayList<>();
     private boolean debug;
     public SortAnalyzer(boolean debug) {
         this.debug = debug;
@@ -14,11 +14,11 @@ public class SortAnalyzer {
      * @param elementCount The number of elements which will be added to the unsortedArray.
      */
     public void insertionSort_setWorstCase(int elementCount) {
-        unsortedArray.clear();
+        this.array.clear();
 
         // The worst-case scenario for the insertion sort is that the array is sorted the wrong way round.
         for (int i = elementCount-1; i >= 0; i--) {
-            this.unsortedArray.add(i);
+            this.array.add(i);
         }
     }
 
@@ -28,26 +28,23 @@ public class SortAnalyzer {
      * @return The previously unsorted array, now sorted.
      */
     public ArrayList<Integer> insertionSort_MoveApproach() {
-        // Copy unsorted array into a new array which will be sorted
-        ArrayList<Integer> sArray = new ArrayList<>(this.unsortedArray);
-
         // Iterate through every element in the array, except the last one.
-        for (int i = 0; i < sArray.size()-1; i++) {
+        for (int i = 0; i < this.array.size()-1; i++) {
             // Get the index of the next element which will be pulled into the sorted section.
             // Temporarily save the next element in nextInt.
             int nextIndex = i + 1;
-            Integer nextInt = sArray.get(nextIndex);
+            Integer nextInt = this.array.get(nextIndex);
 
             // Move all elements to the right, until the right position of nextInt has been found.
-            while (nextIndex > 0 && nextInt < sArray.get(nextIndex-1)) {
-                sArray.set(nextIndex, sArray.get(nextIndex-1));
+            while (nextIndex > 0 && nextInt < this.array.get(nextIndex-1)) {
+                this.array.set(nextIndex, this.array.get(nextIndex-1));
                 nextIndex--;
             }
 
             // Correct position has been found. Place nextInt.
-            sArray.set(nextIndex, nextInt);
-            if (this.debug) System.out.println(sArray);
+            this.array.set(nextIndex, nextInt);
+            if (this.debug) System.out.println(this.array);
         }
-        return sArray;
+        return this.array;
     }
 }
